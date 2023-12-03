4855dacc8eba87a3a0cc2a8813e40aa5b9705d06
Christopher Acosta <chriz@live.dk>
2023-12-03 16:17:33 +0900

    Day 3b

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,15 +4,56 @@ fn main() {
     for line in lines {
         map.push(line.chars().collect());
     }
-    for row in &map {
-        println!("{:?}", row);
-    }
+
+    // Part 1
+    // let mut sum = 0;
+    // let rows = map.len();
+    // let columns = map[0].len();
+    // for i in 0..rows {
+    //     for j in 0..columns {
+    //         if !map[i][j].is_ascii_digit() && map[i][j] != '.' {
+    //             for di in -1..=1 {
+    //                 let mut visited = Vec::new();
+    //                 for dj in -1..=1 {
+    //                     let ii = i as i32 + di;
+    //                     let jj = j as i32 + dj;
+    //                     if (0..rows).contains(&(ii as usize))
+    //                         && (0..columns).contains(&(jj as usize))
+    //                     {
+    //                         if map[ii as usize][jj as usize].is_ascii_digit()
+    //                             && !visited.contains(&jj)
+    //                         {
+    //                             let mut jjj = jj as usize;
+    //                             while jjj > 0 && map[ii as usize][jjj - 1].is_ascii_digit() {
+    //                                 jjj -= 1;
+    //                             }
+    //                             let s: String = map[ii as usize][jjj..]
+    //                                 .iter()
+    //                                 .take_while(|c| c.is_ascii_digit())
+    //                                 .collect();
+    //                             let n: usize = s.parse().unwrap();
+    //                             dbg!(n);
+    //                             sum += n;
+    //                             for idx in 0..s.len() {
+    //                                 visited.push((jjj + idx) as i32);
+    //                             }
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //     }
+    // }
+    // println!("{}", sum);
+
+    // Part 2
     let mut sum = 0;
     let rows = map.len();
     let columns = map[0].len();
     for i in 0..rows {
         for j in 0..columns {
-            if !map[i][j].is_ascii_digit() && map[i][j] != '.' {
+            if map[i][j] == '*' {
+                let mut parts = Vec::new();
                 for di in -1..=1 {
                     let mut visited = Vec::new();
                     for dj in -1..=1 {
@@ -33,14 +74,16 @@ fn main() {
                                     .take_while(|c| c.is_ascii_digit())
                                     .collect();
                                 let n: usize = s.parse().unwrap();
-                                dbg!(n);
-                                sum += n;
+                                parts.push(n);
                                 for idx in 0..s.len() {
                                     visited.push((jjj + idx) as i32);
                                 }
                             }
                         }
                     }
+                }
+                if parts.len() == 2 {
+                    sum += parts.iter().product::<usize>();
                 }
             }
         }
